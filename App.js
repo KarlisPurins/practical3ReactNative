ba67ec32d6d66c1a696b907b1cdddb496a7e4355
KarlisPurins <karlis.purins.1999@gmail.com>
2022-03-22 15:59:04 +0200

    Show maps

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { useState } from 'react/cjs/react.production.min';
 
-export default function App() {
+export default function App (){
+  /*const[isLoading, setLoading] = useState(true);
+  const[data, setData] = useState([]);
+
+  const getMovies = async () =>{
+    try{
+      const response = await fetch('https://reactnative.dev/movies.json');
+      const json = await response.json();
+      setData(json.movies);
+
+    }catch(error){
+      console.error(error);
+    }finally{
+      setLoading(false);
+    }
+  }*/
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} />
     </View>
   );
 }
@@ -16,5 +33,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
