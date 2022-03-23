a2a98c30bd06da41e690473750957788a95f0711
Ivars Sloka <ivis31121999@gmail.com>
2022-03-23 23:52:38 +0200

    Added API requests for extra information

--- App.js
+++ App.js
@@ -6,35 +6,69 @@ import Button from './components/showWeatherButton';
 import * as Location from 'expo-location';
 
 export default function App (){
-  const[isLoading, setLoading] = useState(true);
-  const[data, setData] = useState([]);
+  //const [place, setPlace] = useState([]);
+  const [isLoading, setLoading] = useState(true);
   const [location, setLocation] = useState(null);
+  const [place, setPlace] = useState(null);
+  const [country, setCountry] = useState(null);
+  const [temperature, setTemperature] = useState(null);
+  const [pressure, setPressure] = useState(null);
+  const [humidity, setHumidity] = useState(null);
+  const [description, setDescription] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
 
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
+  function temperatureConverter(valNum) {
+  valNum = parseFloat(valNum);
+  return valNum-273.15;
+  }
+
+  const getPlaceData = async (lat, lon) => {
+    var linkAPI = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=26522379884692fd13fb1ddaacf8975c&lang=la';
+    try {
+      const response = await fetch(linkAPI);
+      const json = await response.json();
+      console.log(json.name);
+      setPlace(json.name);
+      setCountry(json.sys.country);
+      setTemperature(temperatureConverter(json.main.temp).toFixed(2));
+      setPressure(json.main.pressure);
+      setHumidity(json.main.humidity);
+      console.log(json.weather);
+      setDescription(json.weather.description)
+      return json;
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   /*const getWeatherData = () => {
     try {
-      fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}')
+      fetch('https://reactnative.dev/movies.json')
       .then(response => response.json())
       .then(data => setData(data.movies));
     } catch (error) {
       console.error(error);
-    } finally {
-      setLoading(false);
-    }
+    } 
   }*/
 
+  /*const getWeatherData = async () => {
+    linkAPI = 'https://reactnative.dev/movies.json';
+    try {
+      const response = await fetch(linkAPI);
+      const json = await response.json();
+      return json.movies;
+    } catch (error) {
+      console.error(error);
+    }
+  };*/
+
   useEffect(() => {
-    //getWeatherData();
     (async () => {
           let { status } = await Location.requestForegroundPermissionsAsync();
           if (status !== 'granted') {
             setErrorMsg('Permission to access location was denied');
             return;
           }
-
           let location = await Location.getCurrentPositionAsync({});
           setLocation(location);
         })();
@@ -48,27 +82,25 @@ export default function App (){
         var timestampJSON = location['timestamp'];
         var latitudeJSON = location.coords.latitude;
         var longitudeJSON = location.coords.longitude;
+
+        var placeJSON = getPlaceData(latitudeJSON, longitudeJSON);
+        
       }
 
   return (
     <View style={styles.containerView}>
       <MapView style={styles.map} />
       <View style={styles.containerButton}>
-        <Text style={styles.text}>Place: {}</Text>
-        <Text style={styles.text}>Latitude: {latitude}</Text>
-        <Text style={styles.text}>Longitude: {longitude}</Text>
-        <Text style={styles.text}>Temp: {}</Text>
-        <Text style={styles.text}>Pressure: {}</Text>
-        <Text style={styles.text}>Humidity: {}</Text>
-        <Text style={styles.text}>Description: {}</Text>
-      <FlatList
-         //data={data}
-         // keyExtractor={({ id }, index) => id}
-          //renderItem={({ item }) => (
-            // <Text style={styles.text}>{text}</Text>    
-         // )}
-         />
+        <View style={styles.containerButton}>
+            <Text style={styles.text}>Place: {place}, {country}</Text>
+            <Text style={styles.text}>Latitude: {latitudeJSON}</Text>
+            <Text style={styles.text}>Longitude: {longitudeJSON}</Text>
+            <Text style={styles.text}>Temp: {temperature} °C</Text>
+            <Text style={styles.text}>Pressure: {pressure}</Text>
+            <Text style={styles.text}>Humidity: {humidity}</Text>
+            <Text style={styles.text}>Description: {description}</Text>     
         <Button />
+        </View>
       </View>
     </View>
   );
