0acd4bc6eab9916ce80a30dd77b6c5bb86c3a903
Ivars Sloka <ivis31121999@gmail.com>
2022-03-25 15:39:11 +0200

    Added location marker and fixed portrait and landscape views

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import MapView from 'react-native-maps';
 import Marker from 'react-native-maps';
 import Callout from 'react-native-maps';
-import { ActivityIndicator, StyleSheet, Text, View, Dimensions } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location';
 
-export default function App (){
-  //const [place, setPlace] = useState([]);
+export default function App() {
   const [isLoading, setLoading] = useState(true);
   const [location, setLocation] = useState(null);
   const [place, setPlace] = useState(null);
@@ -22,12 +21,13 @@ export default function App (){
   const [longitude, setLongitude] = useState(null);
 
   function temperatureConverter(valNum) {
-  valNum = parseFloat(valNum);
-  return valNum-273.15;
+    valNum = parseFloat(valNum);
+    return valNum - 273.15;
   }
 
   const getPlaceData = async (lat, lon) => {
-    var linkAPI = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=26522379884692fd13fb1ddaacf8975c&lang=la';
+    var linkAPI =
+      'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=26522379884692fd13fb1ddaacf8975c&lang=la';
     try {
       const response = await fetch(linkAPI);
       const json = await response.json();
@@ -46,51 +46,58 @@ export default function App (){
   };
 
   const getCurrentLocation = async () => {
-     let { status } = await Location.requestForegroundPermissionsAsync();
-          if (status !== 'granted') {
-            setErrorMsg('Permission to access location was denied');
-            return;
-          }
-          let location = await Location.getCurrentPositionAsync({});
-          setLocation(location);
-          setLatitude(location.coords.latitude);
-          setLongitude(location.coords.longitude);
-      var text = 'Waiting..';
-      if (errorMsg) {
-        text = errorMsg;
-      }
-  }
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+    setLatitude(location.coords.latitude);
+    setLongitude(location.coords.longitude);
+    var text = 'Waiting..';
+    if (errorMsg) {
+      text = errorMsg;
+    }
+  };
 
   useEffect(() => {
     getCurrentLocation();
-      }, []);
+  }, []);
 
-    getPlaceData(latitude, longitude);
+  getPlaceData(latitude, longitude);
 
   return (
     <View style={styles.containerView}>
-    {isLoading ? <ActivityIndicator/> : (
-      <MapView initialRegion={{
-                latitude: latitude,
-                longitude: longitude,
-                latitudeDelta: 0.1,
-                longitudeDelta: 0.1,
-              }}
-                style={styles.map}>
-                <MapView.Marker style={styles.map} title={place+', '+country} coordinate={{ latitude : latitude , longitude : longitude }} pinColor={'#00BFFF'}>
-                <MapView.Callout>
-                  <View style={{height: 200, width: 250}}>
-                    <Text style={styles.text}>Place: {place}, {country}</Text>
-                    <Text style={styles.text}>Latitude: {latitude}</Text>
-                    <Text style={styles.text}>Longitude: {longitude}</Text>
-                    <Text style={styles.text}>Temp: {temperature}°C</Text>
-                    <Text style={styles.text}>Pressure: {pressure} hPa</Text>
-                    <Text style={styles.text}>Humidity: {humidity}%</Text>
-                    <Text style={styles.text}>Description: {description}</Text>
-                    </View>
-                </MapView.Callout>
-                </MapView.Marker>
-      </MapView>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <MapView
+          initialRegion={{
+            latitude: latitude,
+            longitude: longitude,
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.1,
+          }}
+          style={styles.map}>
+          <MapView.Marker
+            style={styles.map}
+            title={place + ', ' + country}
+            coordinate={{ latitude: latitude, longitude: longitude }}
+            pinColor={'#00BFFF'}>
+            <MapView.Callout>
+              <View style={{ height: 200, width: 250 }}>
+                <Text style={styles.text}>Place: {place}, {country}</Text>
+                <Text style={styles.text}>Latitude: {latitude}</Text>
+                <Text style={styles.text}>Longitude: {longitude}</Text>
+                <Text style={styles.text}>Temp: {temperature}°C</Text>
+                <Text style={styles.text}>Pressure: {pressure} hPa</Text>
+                <Text style={styles.text}>Humidity: {humidity}%</Text>
+                <Text style={styles.text}>Description: {description}</Text>
+              </View>
+            </MapView.Callout>
+          </MapView.Marker>
+        </MapView>
       )}
     </View>
   );
@@ -111,6 +118,6 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   text: {
-    fontSize: 19
-  }
+    fontSize: 19,
+  },
 });
